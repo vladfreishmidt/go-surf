5ef9e8389b498471bea9c87a24665eba6161b1d2
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-01-29 22:03:41 +0200

    header dots slider added

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function(){
 
   $('.header__slider').slick({
-    infinite: true.valueOf,
-    fade: true
+    infinite: true,
+    fade: true,
+    asNavFor: '.slider-dots',
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow right">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow right">',
+  });
+
+  $('.slider-dots').slick({
+    asNavFor: '.header__slider',
+    slidesToShow: 4,
+    slidesToScroll: 4,
   });
 
 
